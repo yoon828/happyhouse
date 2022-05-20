e202bc90eef58b121b690dbbb558f51e69720c05
정지원 <stopone2639@naver.com>
2022-05-20 11:26:04 +0900

    회원 관리 화면 변경
    회원 관리 화면 변경

--- FrontEnd/src/components/member/MemberLogin.vue
+++ FrontEnd/src/components/member/MemberLogin.vue
@@ -1,65 +1,39 @@
 <template>
-  <div class="wrapper">
-    <div class="section page-header header-filter" :style="headerStyle">
-      <div class="container">
-        <div class="md-layout">
-          <div
-            class="md-layout-item md-size-33 md-small-size-66 md-xsmall-size-100 md-medium-size-40 mx-auto"
-          >
-            <login-card header-color="green">
-              <h4 slot="title" class="card-title">로그인</h4>
-              <p slot="description" class="description">
-                happy house에 오신 걸 환영 합니다.
-              </p>
-              <md-field class="md-form-group" slot="inputs">
-                <md-icon>face</md-icon>
-                <label>아이디</label>
-                <md-input v-model="userid"></md-input>
-              </md-field>
-              <md-field class="md-form-group" slot="inputs">
-                <md-icon>lock_outline</md-icon>
-                <label>비밀번호</label>
-                <md-input type="password" v-model="userpw"></md-input>
-              </md-field>
-              <md-button slot="footer" class="md-success">
-                로그인
-              </md-button>
-            </login-card>
-          </div>
-        </div>
-      </div>
+  <div class="container">
+    <div id="box" class="col-md-12 text-center">
+      <h2>로그인</h2>
+      <p slot="description" class="description">
+        happy house에 오신 걸 환영 합니다.
+      </p>
+      <md-field class="md-form-group" slot="inputs">
+        <md-icon>face</md-icon>
+        <label>아이디</label>
+        <md-input v-model="userid"></md-input>
+      </md-field>
+      <md-field class="md-form-group" slot="inputs">
+        <md-icon>lock_outline</md-icon>
+        <label>비밀번호</label>
+        <md-input type="password" v-model="userpw"></md-input>
+      </md-field>
+      <md-button slot="footer" class="md-success">
+        로그인
+      </md-button>
     </div>
   </div>
 </template>
 
 <script>
-import { LoginCard } from "@/components";
-
-export default {
-  components: {
-    LoginCard,
-  },
-  bodyClass: "login-page",
-  data() {
-    return {
-      userid: null,
-      userpw: null,
-    };
-  },
-  props: {
-    header: {
-      type: String,
-      default: require("@/assets/img/house-bg.png"),
-    },
-  },
-  computed: {
-    headerStyle() {
-      return {
-        backgroundImage: `url(${this.header})`,
-      };
-    },
-  },
-};
+export default {};
 </script>
 
-<style lang="css"></style>
+<style>
+#box {
+  padding: 100px;
+  margin: 5px;
+}
+.md-form-group {
+  width: 50%;
+  margin: 0 auto;
+  padding: 15px;
+}
+</style>
--- FrontEnd/src/components/member/MemberRegist.vue
+++ FrontEnd/src/components/member/MemberRegist.vue
@@ -1,77 +1,50 @@
 <template>
-  <div class="wrapper">
-    <div class="section page-header header-filter" :style="headerStyle">
-      <div class="container">
-        <div class="md-layout">
-          <div
-            class="md-layout-item md-size-33 md-small-size-66 md-xsmall-size-100 md-medium-size-40 mx-auto"
-          >
-            <login-card header-color="green">
-              <h4 slot="title" class="card-title">회원 가입</h4>
-              <p slot="description" class="description">
-                happy house에 오신 걸 환영 합니다.
-              </p>
-              <md-field class="md-form-group" slot="inputs">
-                <md-icon>man</md-icon>
-                <label>이름</label>
-                <md-input v-model="username"></md-input>
-              </md-field>
-              <md-field class="md-form-group" slot="inputs">
-                <md-icon>face</md-icon>
-                <label>아이디</label>
-                <md-input v-model="userid"></md-input>
-              </md-field>
-              <md-field class="md-form-group" slot="inputs">
-                <md-icon>lock_outline</md-icon>
-                <label>비밀번호</label>
-                <md-input
-                  type="password"
-                  v-model="userpw"
-                  ref="userpw"
-                ></md-input>
-              </md-field>
-              <md-field class="md-form-group" slot="inputs">
-                <md-icon>email</md-icon>
-                <label>이메일</label>
-                <md-input type="email" v-model="useraddress"></md-input>
-              </md-field>
-              <md-field class="md-form-group" slot="inputs">
-                <md-icon>phone</md-icon>
-                <label>전화 번호</label>
-                <md-input v-model="usernumber"></md-input>
-              </md-field>
+  <div class="container">
+    <div id="box" class="col-md-12 text-center">
+      <h2>회원 가입</h2>
+      <p slot="description" class="description">
+        happy house에 오신 걸 환영 합니다.
+      </p>
+      <md-field class="md-form-group" slot="inputs">
+        <md-icon>man</md-icon>
+        <label>이름</label>
+        <md-input v-model="username"></md-input>
+      </md-field>
+      <md-field class="md-form-group" slot="inputs">
+        <md-icon>face</md-icon>
+        <label>아이디</label>
+        <md-input v-model="userid"></md-input>
+      </md-field>
+      <md-field class="md-form-group" slot="inputs">
+        <md-icon>lock_outline</md-icon>
+        <label>비밀번호</label>
+        <md-input type="password" v-model="userpw" ref="userpw"></md-input>
+      </md-field>
+      <md-field class="md-form-group" slot="inputs">
+        <md-icon>email</md-icon>
+        <label>이메일</label>
+        <md-input type="email" v-model="useraddress"></md-input>
+      </md-field>
+      <md-field class="md-form-group" slot="inputs">
+        <md-icon>phone</md-icon>
+        <label>전화 번호</label>
+        <md-input v-model="usernumber"></md-input>
+      </md-field>
 
-              <md-button
-                slot="footer"
-                class="md-success"
-                v-on:click="registCheck()"
-              >
-                회원 가입
-              </md-button>
+      <md-button slot="footer" class="md-success" v-on:click="registCheck()">
+        회원 가입
+      </md-button>
 
-              <md-button
-                slot="footer"
-                class="md-success"
-                v-on:click="initData()"
-              >
-                초기화
-              </md-button>
-            </login-card>
-          </div>
-        </div>
-      </div>
+      <md-button slot="footer" class="md-success" v-on:click="initData()">
+        초기화
+      </md-button>
     </div>
   </div>
 </template>
 
 <script>
-import { LoginCard } from "@/components";
 import { registMember } from "@/api/member";
 export default {
-  components: {
-    LoginCard,
-  },
-  bodyClass: "login-page",
   data() {
     return {
       username: "",
@@ -162,4 +135,14 @@ export default {
 };
 </script>
 
-<style lang="css"></style>
+<style>
+#box {
+  padding: 100px;
+  margin: 5px;
+}
+.md-form-group {
+  width: 50%;
+  margin: 0 auto;
+  padding: 15px;
+}
+</style>

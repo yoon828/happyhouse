72123b8579fcef34ade12743325ff9bba72dfedb
k915k1004 <k915k1004@naver.com>
2022-05-26 08:50:32 +0900

    삭제

--- BackEnd/happyhouseVue/src/main/java/com/ssafy/happyhouse/config/WebMvcConfiguration.java
+++ BackEnd/happyhouseVue/src/main/java/com/ssafy/happyhouse/config/WebMvcConfiguration.java
@@ -13,21 +13,11 @@ import com.ssafy.happyhouse.interceptor.BearerAuthInterceptor;
 @MapperScan(basePackages = { "com.ssafy.**.mapper" })
 public class WebMvcConfiguration implements WebMvcConfigurer {
 
-	private final BearerAuthInterceptor bearerAuthInterceptor;
-
-	public WebMvcConfiguration(BearerAuthInterceptor bearerAuthInterceptor) {
-		this.bearerAuthInterceptor = bearerAuthInterceptor;
-	}
 
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST", "PUT", "DELETE");
 	}
 
-//	// 인터셉터 등록하기
-//	public void addInterceptors(InterceptorRegistry registry) {
-//		System.out.println(">>> 인터셉터 등록");
-//		registry.addInterceptor(bearerAuthInterceptor)
-//		.addPathPatterns("/userapi/update");
-//	}
+
 }
